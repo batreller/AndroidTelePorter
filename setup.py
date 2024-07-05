a0f922997a83be4172a034a576cc8a8dcd30fc44
batreller <batreller@gmail.com>
2024-07-05 22:36:59 +0100

    update version to 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Serializer and deserializer for mobile telegram session'
 
 setup(
