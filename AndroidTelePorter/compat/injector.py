2ab464a05a68f6c0aa2982fc3fd58a7de879974f
batreller <batreller@gmail.com>
2026-05-31 16:12:12 +0100

    remove unused tree variable

--- AndroidTelePorter/compat/injector.py
+++ AndroidTelePorter/compat/injector.py
@@ -211,7 +211,6 @@ def read_user_config(data: bytes) -> UserConfig:
         TypeNotFoundError: a constructor id (root or nested) is not present in
             ``data/legacy.tl`` and is unknown to Telethon.
     """
-    tree = read_user(data)
     obj = read_user(data)
     tree = obj.to_dict()
     return UserConfig(
